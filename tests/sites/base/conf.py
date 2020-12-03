c84a28d956bd428064d24f93856fdcfbe010ba03
AakashGC <aakashgfude@gmail.com>
2020-12-03 14:32:41 +1100

    just adding a fake nb_repo_url for coverage testing

--- tests/sites/base/conf.py
+++ tests/sites/base/conf.py
@@ -22,6 +22,7 @@ jupyter_execute_notebooks = "auto"
 html_theme_options = {
     "path_to_docs": "TESTPATH",
     "repository_url": "https://github.com/executablebooks/sphinx-book-theme",
+    "nb_repository_url": "https://github.com/executablebooks/sphinx-book-theme",
     # "repository_branch": "master",  # Not using this, should default to master
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",
