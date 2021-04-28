1e9430dd439366ec48a60274ae7f61a5f562106a
AakashGC <aakashgfude@gmail.com>
2021-04-28 16:16:21 +1000

    some edits

--- quantecon_book_theme/__init__.py
+++ quantecon_book_theme/__init__.py
@@ -1,5 +1,12 @@
 """A lightweight book theme based on the pydata sphinx theme."""
 from pathlib import Path
+
+try:
+    import importlib.resources as resources
+except ImportError:
+    # python < 3.7
+    import importlib_resources as resources
+
 from docutils.parsers.rst import directives
 from docutils import nodes
 from sphinx.util import logging
@@ -7,13 +14,16 @@ from sphinx.util.fileutil import copy_asset
 from sphinx.util.osutil import ensuredir
 from bs4 import BeautifulSoup as bs
 from sass import compile as sass_compile
+import os
 
+from . import static as theme_static
 from .launch import add_hub_urls
 
 __version__ = "0.1.5"
 """quantecon-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
+MESSAGE_CATALOG_NAME = "booktheme"
 
 
 def get_html_theme_path():
@@ -22,7 +32,54 @@ def get_html_theme_path():
     return theme_path
 
 
+def add_static_paths(app):
+    """Ensure CSS/JS is loaded."""
+    app.env.book_theme_resources_changed = False
+
+    output_static_folder = Path(app.outdir) / "_static"
+    theme_static_files = resources.contents(theme_static)
+
+    if (
+        app.config.html_theme_options.get("theme_dev_mode", False)
+        and output_static_folder.exists()
+    ):
+        # during development, the JS/CSS may change, if this is the case,
+        # we want to remove the old files and ensure that the new files are loaded
+        for path in output_static_folder.glob("quantecon-book-theme*"):
+            if path.name not in theme_static_files:
+                app.env.book_theme_resources_changed = True
+                path.unlink()
+        # note sphinx treats theme css different to regular css
+        # (it is specified in theme.conf), so we don't directly use app.add_css_file
+        for fname in resources.contents(theme_static):
+            if fname.endswith(".css"):
+                if not (output_static_folder / fname).exists():
+                    (output_static_folder / fname).write_bytes(
+                        resources.read_binary(theme_static, fname)
+                    )
+                    app.env.book_theme_resources_changed = True
+
+    # add javascript
+    for fname in resources.contents(theme_static):
+        if fname.endswith(".js"):
+            app.add_js_file(fname)
+
+
+def update_all(app, env):
+    """During development, if CSS/JS has changed, all files should be re-written,
+    to load the correct resources.
+    """
+    if (
+        app.config.html_theme_options.get("theme_dev_mode", False)
+        and env.book_theme_resources_changed
+    ):
+        return list(env.all_docs.keys())
+
+
 def add_static_path(app):
+    """Ensure CSS/JS is loaded."""
+    app.env.book_theme_resources_changed = False
+
     static_path = Path(__file__).parent.joinpath("static").absolute()
     app.config.html_static_path.append(str(static_path))
 
@@ -67,10 +124,13 @@ def find_url_relative_to_root(pagename, relative_page, path_docs_source):
 
 def add_to_context(app, pagename, templatename, context, doctree):
     def generate_nav_html(
-        level=1,
-        include_item_names=False,
+        startdepth=1,
+        kind="sidebar",
+        maxdepth=4,
+        collapse=False,
+        includehidden=True,
+        titles_only=True,
         with_home_page=False,
-        prev_section_numbers=None,
     ):
         # Config stuff
         config = app.env.config
@@ -94,12 +154,17 @@ def add_to_context(app, pagename, templatename, context, doctree):
             master_doctree = app.env.get_doctree(master_doc)
             master_url = context["pathto"](master_doc)
             master_title = list(master_doctree.traverse(nodes.title))[0].astext()
-
+            if len(master_title) == 0:
+                raise ValueError(f"Landing page missing a title: {master_doc}")
+            master_title = master_title[0].astext()
+            li_class = "toctree-l1"
+            if context["pagename"] == master_doc:
+                li_class += " current"
             # Insert it into our toctree
             ul_home = bs(
                 f"""
-            <ul>
-                <li class="toctree-l1">
+            <ul class="nav bd-sidenav">
+                <li class="{li_class}">
                     <a href="{master_url}" class="reference internal">{master_title}</a>
                 </li>
             </ul>""",
@@ -327,15 +392,22 @@ class Margin(directives.body.Sidebar):
 
 
 def setup(app):
-    app.connect("env-before-read-docs", update_thebe_config)
-
     # Configuration for Juypter Book
     app.connect("html-page-context", add_hub_urls)
+    app.connect("env-updated", update_all)
 
-    app.connect("builder-inited", add_static_path)
+    # add translations
+    package_dir = os.path.abspath(os.path.dirname(__file__))
+    locale_dir = os.path.join(package_dir, "translations", "locales")
+    app.add_message_catalog(MESSAGE_CATALOG_NAME, locale_dir)
 
     app.add_html_theme("quantecon_book_theme", get_html_theme_path())
     app.connect("html-page-context", add_to_context)
 
     app.add_js_file("quantecon-book-theme.js")
     app.add_directive("margin", Margin, override=True)
+
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
