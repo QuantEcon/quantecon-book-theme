9af1b22154b8d6b8c995081c07357b1abdc22764
AakashGC <aakashgfude@gmail.com>
2021-08-06 13:56:11 +1000

    version increase

--- quantecon_book_theme/__init__.py
+++ quantecon_book_theme/__init__.py
@@ -10,7 +10,7 @@ from sphinx.util.osutil import ensuredir
 
 from .launch import add_hub_urls
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 """quantecon-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
