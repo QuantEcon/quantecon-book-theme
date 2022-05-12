91f2ec93583d9fc50010ab06bbdffb2586008437
AakashGC <aakashguptachoudhury@gmail.com>
2022-05-12 11:21:43 +0530

    fixing sphinx versions

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ setup(
     install_requires=[
         "pyyaml",
         "docutils>=0.15",
-        "sphinx",
         "click",
         "setuptools",
         "libsass~=0.20.1",
+        "sphinx_book_theme~=0.1.0",
         "beautifulsoup4",
         "web-compile",
         "markupsafe~=2.0.1",
-        "sphinx_book_theme~=0.1.0",
+        "sphinx<5,>=3",
         'importlib-resources>=3.0,<3.5; python_version < "3.7"',
     ],
     extras_require={
