69418e6728e653fe0521dc6b7ba1de406bcadeee
mmcky <mmcky@users.noreply.github.com>
2023-06-15 11:51:31 +1000

    RELEASE: v0.5.1 (#213)

--- quantecon_book_theme/__init__.py
+++ quantecon_book_theme/__init__.py
@@ -10,7 +10,7 @@ from sphinx.util.osutil import ensuredir
 
 from .launch import add_hub_urls
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 """quantecon-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
