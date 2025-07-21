9cc39eccb549f41d0b711eee33e5b330e7312cab
mmcky <mamckay@gmail.com>
2025-07-21 13:40:44 +1000

    MAINT: enable compatibility with sphinx>=7,<8

--- src/quantecon_book_theme/__init__.py
+++ src/quantecon_book_theme/__init__.py
@@ -12,7 +12,7 @@ from sphinx.util.osutil import ensuredir
 
 from .launch import add_hub_urls
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 """quantecon-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
