5a65ef5f5bcb0a82047fc633ddc243a8f6a72600
mmcky <mamckay@gmail.com>
2025-08-11 17:10:19 +1000

    upgrade version to 0.9.0

--- src/quantecon_book_theme/__init__.py
+++ src/quantecon_book_theme/__init__.py
@@ -12,7 +12,7 @@ from sphinx.util.osutil import ensuredir
 
 from .launch import add_hub_urls
 
-__version__ = "0.8.3"
+__version__ = "0.9.0"
 """quantecon-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
