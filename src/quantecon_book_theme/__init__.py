ca79b688eb2582013b9cd0e8333c5573deb39d6d
mmcky <mamckay@gmail.com>
2025-09-02 12:01:49 +1000

    RLS: setup for v0.9.1

--- src/quantecon_book_theme/__init__.py
+++ src/quantecon_book_theme/__init__.py
@@ -13,7 +13,7 @@ from sphinx.util.osutil import ensuredir
 
 from .launch import add_hub_urls
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 """quantecon-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
